e78992d5b2ccc5a0dbfe31f07ea17252b31395a9
toheedakhtar <toheedak333@gmail.com>
2024-06-30 14:21:01 +0530

    displayed content of opf

--- epub3/epub.py
+++ epub3/epub.py
@@ -4,18 +4,16 @@ import xml.etree.ElementTree as ET
 
 # getting to ocf file
 
-main_path = '/home/toheed/Projects/epub_parse/epub3/'
-epub_path = '/home/toheed/Projects/epub_parse/moby-dick.epub'
-epub_name = epub_path.split('/')[-1].split('.')[0]
+main_path = '/home/toheed/Projects/epub_parse/epub3/'   # where epub is stored
+epub_path = '/home/toheed/Projects/epub_parse/linear-algebra.epub'   # epub path
+epub_name = epub_path.split('/')[-1].split('.')[0]      # epub name
 
 # unzipping epub file
 
 with ZipFile(epub_path) as zipobj:
     zipobj.extractall(path = str(epub_name))
 
-#dir_name = 'moby-dick'          # epub dir  
-
-dir_name = epub_name
+dir_name = epub_name            # where epub is extracted , rn where epub is
 
 md_dir = os.listdir(dir_name)
 if 'META-INF' in md_dir:
@@ -33,20 +31,21 @@ if 'META-INF' in md_dir:
                 data = f.read()
 
 
+#xml
 # getting .ocf path from container
 root = ET.fromstring(str(data))
 # Define the namespace
 namespace = {'ns': 'urn:oasis:names:tc:opendocument:xmlns:container'}
-
 # Find the rootfile element and extract the full-path attribute value
 full_path = root.find('.//ns:rootfile', namespace).attrib['full-path']
 
-opf_path = dir_name + '/' + full_path
-print(opf_path)
 
-opf_dir = main_path + opf_path
-print(opf_dir)
+opf_path_extract = dir_name + '/' + full_path   # inside extracted epub  
+#print(opf_path_extract)
 
-with open(opf_dir) as opf:
+opf_path_dir = main_path + opf_path_extract          # from main path
+#print(opf_path_dir)
+
+with open(opf_path_dir) as opf:
     data = opf.read()
     print(data)
